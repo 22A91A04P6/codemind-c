144428f5d8e548c662820ee1f36c2f9a8561ae05
22A91A04P6 <119720503+22A91A04P6@users.noreply.github.com>
2022-12-20 18:49:37 +0530

    #include<stdio.h> int main() {     int p1,p2,p3,p4;     scanf("%d%d%d%d",&p1,&p2,&p3,&p4);     if((p1,p2,p3,p4)>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int p1,p2,p3,p4;
+    scanf("%d%d%d%d",&p1,&p2,&p3,&p4);
+    if((p1,p2,p3,p4)>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
