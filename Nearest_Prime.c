3ab2d88d2905228c3b17e74bda013e3ed753aefd
22A91A04P6 <119720503+22A91A04P6@users.noreply.github.com>
2022-12-21 18:55:21 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],j,k,np,sp,c=0,d,e;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=arr[i];j>=arr[i]-10;j--)         {         	c=0;             for(k=1;k<=j;k++)             {                 if(j%k==0)                 {                     c++;                 }             }             if(c==2)             {                 np=j;                 d=arr[i]-np;                 break;             }         } for(j=arr[i];j<=arr[i]+10;j++)         {         	c=0;             for(k=1;k<=j;k++)             {                 if(j%k==0)                 {                     c++;                 }             }             if(c==2)             {                 sp=j;                 e=sp-arr[i];                 break;             }         }         if(d<e || d==e)         {             printf("%d ",np);         }         else {             printf("%d ",sp);         }     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],j,k,np,sp,c=0,d,e;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=arr[i];j>=arr[i]-10;j--)
+        {
+        	c=0;
+            for(k=1;k<=j;k++)
+            {
+                if(j%k==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                np=j;
+                d=arr[i]-np;
+                break;
+            }
+        }
+for(j=arr[i];j<=arr[i]+10;j++)
+        {
+        	c=0;
+            for(k=1;k<=j;k++)
+            {
+                if(j%k==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                sp=j;
+                e=sp-arr[i];
+                break;
+            }
+        }
+        if(d<e || d==e)
+        {
+            printf("%d
+",np);
+        }
+        else
+{
+            printf("%d
+",sp);
+        }
+    }
+}
