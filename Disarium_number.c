143d93142e92bb15d9cd124063b3c9d0626c0143
22A91A04P6 <119720503+22A91A04P6@users.noreply.github.com>
2022-12-21 19:54:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int m,n,s,sq,r,l,c;     scanf("%d",&n);     m=n;     l=n;     c=0;     while(m>0)     {         r=m%10;         m/=10;         c++;     }     s=0;     while(l>0)     {         r=l%10;         l/=10;         sq=pow(r,c);         c--;         s+=sq;     }     if(s==n)     {         printf("True");  }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int m,n,s,sq,r,l,c;
+    scanf("%d",&n);
+    m=n;
+    l=n;
+    c=0;
+    while(m>0)
+    {
+        r=m%10;
+        m/=10;
+        c++;
+    }
+    s=0;
+    while(l>0)
+    {
+        r=l%10;
+        l/=10;
+        sq=pow(r,c);
+        c--;
+        s+=sq;
+    }
+    if(s==n)
+    {
+        printf("True");
+ }
+    else
+    {
+        printf("False");
+    }
+}
